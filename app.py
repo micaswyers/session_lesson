cb6d5d3d585bb2554893c8a24a4cc91c30fd6964
Hackbright Student <user@chromebox-005.(none)>
2013-10-21 15:29:09 -0700

    Updated --> working with SQL database

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
+import sqlite3
+
+DB = None
+CONN = None
 
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    if session.get("username"):
+    if session.get('username'):
         return "User %s is logged in!"%session['username']
     else: 
         return render_template("index.html")
+
+@app.route("/user/<username>")
+def view_user(username):
+    user_id = model.get_user_by_name(username)
+    user_wall_posts = model.get_wall_posts(user_id)
+    return render_template("wall.html", user_id=user_id, author_id=user_wall_posts[2], created_at=user_wall_posts[3], content=user_wall_posts[4])
 
 @app.route("/clear")
 def clear():
--- model.py
+++ model.py
@@ -1,8 +1,32 @@
+import sqlite3
+
+DB = None
+CONN = None
+
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
 
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
 def authenticate(username, password):
-    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-        return ADMIN_USER
-    else:
-        return False
+    connect_to_db()
+    query = """SELECT * FROM users WHERE username = ?"""
+    DB.execute(query, (username, ))
+    row = DB.fetchone()
+    if hash(password) == row[2]:
+        return row[1]
+
+def get_user_by_name(username):
+    query = """SELECT * FROM users WHERE username = ?"""
+    DB.execute(query, (username, ))
+    rows = DB.fetchall()
+    return rows[0]
+
+def get_wall_posts(user_id):
+    query = """SELECT * FROM wall_posts WHERE id = ?"""
+    DB.execute(query, (user_id, ))
+    rows = DB.fetchall()
+    return rows
